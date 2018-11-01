f63ecb707a72908c3a21bf04966f3cdf15e72510
alsm0918 <Smirnovaleksandr14@gmail.com>
2018-11-01 17:45:37 +0300

    213

---
+++ src/main/java/com/netcracker/openshift/MyConfig.java
@@ -0,0 +1,22 @@
+package com.netcracker.openshift;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "bean")
+public class MyConfig {
+
+    private String message = "a message that can be changed live";
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    // getter and setters
+
+}
--- src/main/java/com/netcracker/openshift/OpenshiftApplication.java
+++ src/main/java/com/netcracker/openshift/OpenshiftApplication.java
@@ -1,51 +1,103 @@
 package com.netcracker.openshift;
 
 import io.fabric8.kubernetes.api.model.*;
-import io.fabric8.kubernetes.client.dsl.NonNamespaceOperation;
-import io.fabric8.kubernetes.client.dsl.Resource;
-import io.fabric8.openshift.api.model.*;
+import io.fabric8.kubernetes.api.model.ConfigBuilder;
+import io.fabric8.kubernetes.client.*;
+import io.fabric8.kubernetes.client.Config;
+import io.fabric8.openshift.api.model.DeploymentConfig;
+import io.fabric8.openshift.api.model.Route;
 import io.fabric8.openshift.client.DefaultOpenShiftClient;
 import io.fabric8.openshift.client.OpenShiftClient;
+import io.fabric8.openshift.client.OpenShiftConfig;
+import io.fabric8.openshift.client.OpenShiftConfigBuilder;
+import io.fabric8.openshift.client.osgi.ManagedOpenShiftClient;
+import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class OpenshiftApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(OpenshiftApplication.class, args);
-        //        IClient client = new ClientBuilder("https://console.starter-ca-central-1.openshift.com").usingToken("asfdsfd8a70a3qrfafdsadsf786324").build();
-               /* .withUserName("openshiftdev")
-                .withPassword("wouldntUlik3T0kn0w")
-                .build();*/
-//        client.getAuthorizationContext().setToken("asfdsfd8a70a3qrfafdsadsf786324");
-//        List<IResource> list = client.list(ResourceKind.CONFIG_MAP, "nc-project");
-        //"https://sdnoshm05.netcracker.com:8443/
-//        OpenShiftConfig osConfig = new OpenShiftConfigBuilder()
-//       OpenShiftClient osClient = new DefaultOpenShiftClient(osConfig);
-        
-        OpenShiftClient osClient = new DefaultOpenShiftClient();
-        NonNamespaceOperation<Project, ProjectList, DoneableProject, Resource<Project, DoneableProject>> projects = osClient.projects();
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
-        //config.
-        List<Pod> items = osClient.pods().list().getItems();
-        List<ConfigMap> items1 = osClient.configMaps().list().getItems();
-        Pod pod = osClient.pods().list().getItems().get(2);
-        Container container = pod.getSpec().getContainers().get(0);
-        DeploymentConfig deploymentConfig = osClient.deploymentConfigs().list().getItems().get(0);
-        DeploymentConfigSpec spec = deploymentConfig.getSpec();
-        Probe livenessProbe = container.getLivenessProbe();
-        PodList pods = osClient.pods().list();
-        for (Pod pod_ : pods.getItems()) {
-            String name = pod_.getMetadata().getName();
-            String ip = pod_.getStatus().getPodIP();
+    public static void main(String[] args) {
+
+
+
+        SpringApplication.run(OpenshiftApplication.class, args);
+
+        //final String OAUTH_TOKEN = "jM7LaAuvh0TFQHpkYtB7qFV5TYxPLEUfHF3Yd-hvYjc";
+        final String OAUTH_TOKEN = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJ1bm0tcWEiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlY3JldC5uYW1lIjoiYWRtaW4tYWNjb3VudC10b2tlbi04OTRxZCIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50Lm5hbWUiOiJhZG1pbi1hY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQudWlkIjoiMTFlY2U3NmMtZDA3Ny0xMWU4LWE5MTQtMDAxNjNlMTQ2NmU2Iiwic3ViIjoic3lzdGVtOnNlcnZpY2VhY2NvdW50OnVubS1xYTphZG1pbi1hY2NvdW50In0.pJXofMZXtYKb2E73HRGmAA6pxOlFZ-wBmPa5yGT2FXGKRWBLTeZ08USL9C9GvCkDhnRR08I7J5yMysyp4T4x7GXhF0F3IjNFOLmbEcB-58SUpmjPxE0wrzRwGD6sgz4ekeorClutGRwR-zw9f_d19HJIRawSgXVC46sl2SSkqLHENSwJ5cKKIHogSDBy-igio8PWy4MCIFoyt-Hq45v8LvV0cSyJKpURNx-JwdBWzWRyIcsKG7jBT_WUUKKHoXc-u93BS2svFfT3qkZAm1-LiMayMI4hZXGV0weNN7jCzx6yZdoY070v337x_dy4WPTs0zuOUS0zPBIjKLxz2gMHkg";
+        final String OPENSHIFT_NAMESPACE = "unm-qa";
+        //final String OPENSHIFT_URL = "https://sdnoshm05.netcracker.com:8443/api/v1/";
+        final String OPENSHIFT_MASTER_URL = "https://sdnoshm05.netcracker.com:8443";
+
+        OpenShiftConfig osConfig = new OpenShiftConfigBuilder()
+                .withMasterUrl(OPENSHIFT_MASTER_URL)
+                .withNamespace(OPENSHIFT_NAMESPACE)
+                .withOauthToken(OAUTH_TOKEN)
+                //.withOpenShiftUrl(OPENSHIFT_URL)
+                .withTrustCerts(true)
+                .build();
+        List<Pod> pods = null;
+        List<Service> services = null;
+        List<Route> routes = null;
+        List<DeploymentConfig> deplConfigs = null;
+
+
+
+        try (OpenShiftClient osClient = new DefaultOpenShiftClient(osConfig)) {
+            pods = osClient.pods().list().getItems();
+            services = osClient.services().list().getItems();
+            routes = osClient.routes().list().getItems();
+            deplConfigs = osClient.deploymentConfigs().list().getItems();
+            System.out.println();
+        } catch (KubernetesClientException e) {
+            //log.info("KubernetesClientException", e);
         }
 
+        List<Pod> yangBasedStoragePod = pods.stream()
+                .filter(s -> s.getMetadata().getName().equals("yang-based-storage"))
+                .collect(Collectors.toList());
 
-//        Config config = new ConfigBuilder().withMasterUrl("https://mymaster.com").build();
-//        OpenShiftClient client = new DefaultKubernetesClient(config);*/
-        //System.out.println("test");
+        List<Service> yangBasedStorageService = services.stream()
+                //.filter(s -> s.getMetadata().getName().equals("yang-based-storage"))
+                .filter(s -> s.getMetadata().getName().equals("api-gateway"))
+                .collect(Collectors.toList());
+
+        List<Route> yangBasedStorageRoute = routes.stream()
+                .filter(s -> s.getMetadata().getName().equals("yang-based-storage"))
+                .collect(Collectors.toList());
+
+        List<DeploymentConfig> yangBasedStorageDeploymentConfig = deplConfigs.stream()
+                .filter(s -> s.getMetadata().getName().equals("yang-based-storage"))
+                .collect(Collectors.toList());
+
+        //log.info("Printing Pods healthchecks:");
+        if (pods != null) {
+            for (Pod pod : pods) {
+                List<Container> containers = pod.getSpec().getContainers();
+                for (Container container : containers) {
+                    if (container.getLivenessProbe() != null) {
+                        String name = container.getName();
+                        String livinessProbe = container.getLivenessProbe().getHttpGet().getPath();
+                        String port = container.getLivenessProbe().getHttpGet().getPort().getIntVal().toString();
+                        //String message = "Container name = " + name + " : Liveness probe = " + livinessProbe;
+                        String message = "HEALTH ENDPOINT: https://"+ name + ":" + port + livinessProbe;
+                        System.out.println(message);
+                        //log.info(message);
+                    }
+                }
+            }
+        }
     }
 }
